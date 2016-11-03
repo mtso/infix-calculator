78c109b2c1b37b9768f2919e4c6c6e96c810b682
mtso <mtso@mail.com>
2016-11-02 17:52:53 -0700

    Hook test project

---
+++ InfixCalculator/Main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(int argc, char** argv)
+{
+	string infixExp;
+
+	cout << "Echo an infix expression: ";
+	cin >> infixExp;
+
+	cout << infixExp << endl;
+
+	system("pause");
+	return 0;
+}
