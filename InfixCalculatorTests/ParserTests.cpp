802031a9c00a48a7710728300b02467658bdf2e5
mtso <mtso@mail.com>
2016-11-04 22:10:44 -0700

    Make parser testing better

--- InfixCalculatorTests/ParserTests.cpp
+++ InfixCalculatorTests/ParserTests.cpp
@@ -12,19 +12,40 @@ namespace InfixCalculatorTests
 
 		TEST_METHOD(ParseExpression)
 		{
-			// TODO: Your test code here
-			Assert::IsFalse(Parser::isSyntacticallyCorrect(""));
-			Assert::IsTrue(Parser::isSyntacticallyCorrect("1+1"));
-			
-			char* test = "1++";
-			Assert::IsFalse(Parser::isSyntacticallyCorrect(test));
+			const int EXP_ARRLEN = 10;
 
+			char* trueExp[EXP_ARRLEN];
+			char* falseExp[EXP_ARRLEN];
 
-			Assert::IsFalse(Parser::isSyntacticallyCorrect("1+3(4/5)"));
-			Assert::IsTrue(Parser::isSyntacticallyCorrect("1+(3+4/5)*3"));
+			trueExp[0] = "1+1";
+			trueExp[1] = "4+(4*4)";
+			trueExp[2] = "1+(3+4/5)*3";
+			trueExp[3] = "(1+(3+4/5))*3";
+			trueExp[4] = "(1)-(3+(4/5))-9";
+			trueExp[5] = "1+1";
+			trueExp[6] = "1+1";
+			trueExp[7] = "1+1";
+			trueExp[8] = "1+1";
+			trueExp[9] = "1+1";
 
-			Assert::IsTrue(Parser::isSyntacticallyCorrect("(1+(3+4/5))*3"));
+			falseExp[0] = "";
+			falseExp[1] = "2++";
+			falseExp[2] = "**3";
+			falseExp[3] = "(1)+(3+4/5))*3";
+			falseExp[4] = "(1)-(3+)-9";			// < TODO returns true
+			falseExp[5] = "1+3(4/5)";
+			falseExp[6] = "(1+)-(3+(4/5))-9";	// < TODO returns true
+			falseExp[7] = "";
+			falseExp[8] = "";
+			falseExp[9] = "";
 
+			for (int i = 0; i < EXP_ARRLEN; i++) {
+				Assert::IsTrue(Parser::isSyntacticallyCorrect(trueExp[i]));
+			}
+
+			for (int i = 0; i < EXP_ARRLEN; i++) {
+				Assert::IsFalse(Parser::isSyntacticallyCorrect(falseExp[i]));
+			}
 		}
 
 	};
