1f97fcac544c78ee6017006a2f7fd898391cd684
CodeTortoise <marroquin.age@gmail.com>
2016-11-04 21:41:28 -0700

    Add Operator.h, Operator.cpp, precedence map

---
+++ InfixCalculator/Operator.cpp
@@ -0,0 +1,13 @@
+#include "Operator.h"
+#include <string>
+#include <map>
+
+Operator::Operator()
+{
+	precedences['+'] = 1;
+	precedences['-'] = 1;
+	precedences['*'] = 2;
+	precedences['/'] = 2;
+	precedences['('] = 3;
+	precedences[')'] = 3;
+}
---
+++ InfixCalculator/Stack.cpp
@@ -0,0 +1,52 @@
+// Stack.cpp
+// Lab 2: Stack ADT
+// CIS 22C F2016: Adrian Marroquin, Matthew Tso
+
+#ifndef STACKADT_STACK_CPP
+#define STACKADT_STACK_CPP
+
+#include "Stack.h"
+
+template <typename DataType>
+Stack<DataType>::~Stack()
+{
+	// Do we need to explicitly call?
+
+	util::log("~Stack called");
+}
+
+template <typename DataType>
+bool Stack<DataType>::isEmpty() const
+{
+	// Return superclass's isEmpty function.
+	return List<DataType>::isEmpty();
+}
+
+template <typename DataType>
+int Stack<DataType>::getLength() const
+{
+	// Return superclass's getLength function.
+	return List<DataType>::getLength();
+}
+
+template <typename DataType>
+void Stack<DataType>::clear()
+{
+	List<DataType>::clear();
+}
+
+template <typename DataType>
+bool Stack<DataType>::push(const DataType& item)
+{
+	return addFirst(item);
+}
+
+template <typename DataType>
+DataType Stack<DataType>::pop()
+{
+	DataType value = head->getData();
+	List<DataType>::removeFirst();
+	return value;
+}
+
+#endif
