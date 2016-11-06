243c84301b0221313747451bca9230e9b2a473c1
mtso <mtso@tuta.io>
2016-11-05 23:07:56 -0700

    Add some longer expressions to calculator test

--- InfixCalculatorTests/InfixCalculatorTests.cpp
+++ InfixCalculatorTests/InfixCalculatorTests.cpp
@@ -36,12 +36,40 @@ namespace InfixCalculatorTests
 			infcalc.setInfixExp("3*(3+3)*3");
 			Assert::AreEqual(54, infcalc.getResult());
 		}
-
+		
 		TEST_METHOD(Evaluate1m3p4d5m9)
 		{
 			InfixCalculator infcalc;
 			infcalc.setInfixExp("(1)-(3+(4/5))-9");
 			Assert::AreEqual(-11, infcalc.getResult());
+		}
+		
+		TEST_METHOD(Evaluate1p3p4d5x3)
+		{
+			InfixCalculator infcalc;
+			infcalc.setInfixExp("1+(3+4/5)*3");
+			Assert::AreEqual(10, infcalc.getResult());
+		}
+
+		TEST_METHOD(Evaluate1p3p4d5x3withExtraParens)
+		{
+			InfixCalculator infcalc;
+			infcalc.setInfixExp("(1+(3+4/5))*3");
+			Assert::AreEqual(12, infcalc.getResult());
+		}
+
+		TEST_METHOD(Evaluate1p1x3d4m6p2d6x3p9m8d7d5p3m2m1)
+		{
+			InfixCalculator infcalc;
+			infcalc.setInfixExp("((1+1*(3/4-(6+2/6*3)+(9-8/7))/5+3)-2)*1");
+			Assert::AreEqual(2, infcalc.getResult());
+		}
+
+		TEST_METHOD(Evaluate1p1m0x3d4m6p2d6x3p0x9m8d7d5p3m2m1)
+		{
+			InfixCalculator infcalc;
+			infcalc.setInfixExp("((1+1-0*(3/4-(6+2/6*3)+(0*(9-8)/7))/5+3)-2)*1");
+			Assert::AreEqual(3, infcalc.getResult());
 		}
 	};
 }
