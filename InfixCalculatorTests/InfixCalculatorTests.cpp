d6ea1d25fdb40559871fe1b792690349e48fa42d
mtso <mtso@tuta.io>
2016-11-05 22:05:01 -0700

    Add hard, failing tests to calculator

--- InfixCalculatorTests/InfixCalculatorTests.cpp
+++ InfixCalculatorTests/InfixCalculatorTests.cpp
@@ -10,11 +10,25 @@ namespace InfixCalculatorTests
 	{
 	public:
 
-		TEST_METHOD(EvaluateExpression)
+		TEST_METHOD(Evaluate1p1)
 		{
 			InfixCalculator infcalc;
 			infcalc.setInfixExp("1+1");
 			Assert::AreEqual(2, infcalc.getResult());
+		}
+
+		TEST_METHOD(Evaluate3x3)
+		{
+			InfixCalculator infcalc;
+			infcalc.setInfixExp("3x3");
+			Assert::AreEqual(9, infcalc.getResult());
+		}
+
+		TEST_METHOD(Evaluate3x3p3x3)
+		{
+			InfixCalculator infcalc;
+			infcalc.setInfixExp("3x(3+3)x3");
+			Assert::AreEqual(54, infcalc.getResult());
 		}
 
 	};
